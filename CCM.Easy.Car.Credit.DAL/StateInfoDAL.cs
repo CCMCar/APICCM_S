8b241d3275448f6f5663374328c37a0aab030627
王良昊 <@DESKTOP-BR31NAV>
2019-12-27 20:09:25 +0800

    王良昊 12月27日8点修改

--- CCM.Easy.Car.Credit.DAL/StateInfoDAL.cs
+++ CCM.Easy.Car.Credit.DAL/StateInfoDAL.cs
@@ -12,13 +12,18 @@ namespace CCM.Easy.Car.Credit.DAL
     public class StateInfoDAL
     {
         SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=Project;Integrated Security=True");
-
+        /// <summary>
+        /// 王良昊 12月27修改
+        /// </summary>
+        /// <param name="Id"></param>
+        /// <param name="State"></param>
+        /// <returns></returns>
         public DataTable Collect(int Id, int State)
         {
             if (State == 4)
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand($"select GETDATE(),CarInfo.CarId,CarInfo.CarImg,CarInfo.CarName,CarInfo.CarPrice,City.CityName from CarInfo join BrandType on BrandType.BrandId = CarInfo.BrandId join ShopInfo on CarInfo.ShopId = ShopInfo.ShopId join City on City.CityId = ShopInfo.CityId where CarId={Id} and CarState={State}", conn);
+                SqlCommand cmd = new SqlCommand($"select CarJoinTime,CarInfo.CarId,CarInfo.CarImg,CarInfo.CarName,CarInfo.CarPrice,City.CityName from CarInfo join BrandType on BrandType.BrandId = CarInfo.BrandId join ShopInfo on CarInfo.ShopId = ShopInfo.ShopId join City on City.CityId = ShopInfo.CityId join UserCar on UserCar.CarId = CarInfo.CarId where CarId={Id} and CarState={State}", conn);
                 SqlDataAdapter Sqldata = new SqlDataAdapter(cmd);
                 DataTable dt = new DataTable("lh");
                 Sqldata.Fill(dt);
@@ -28,7 +33,7 @@ namespace CCM.Easy.Car.Credit.DAL
             else
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand($"select GETDATE(),CarInfo.CarId,CarInfo.CarImg,CarInfo.CarName,CarInfo.CarPrice,City.CityName from CarInfo join BrandType on BrandType.BrandId = CarInfo.BrandId join ShopInfo on CarInfo.ShopId = ShopInfo.ShopId join City on City.CityId = ShopInfo.CityId where CarId={Id}  and CarState={State}", conn);
+                SqlCommand cmd = new SqlCommand($"select CarJoinTime,CarInfo.CarId,CarInfo.CarImg,CarInfo.CarName,CarInfo.CarPrice,City.CityName from CarInfo join BrandType on BrandType.BrandId = CarInfo.BrandId join ShopInfo on CarInfo.ShopId = ShopInfo.ShopId join City on City.CityId = ShopInfo.CityId join UserCar on UserCar.CarId = CarInfo.CarId where CarId={Id}  and CarState={State}", conn);
                 SqlDataAdapter Sqldata = new SqlDataAdapter(cmd);
                 DataTable dt = new DataTable("lh");
                 Sqldata.Fill(dt);
